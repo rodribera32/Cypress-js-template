c5987c385263ada7365706bf415977eb1e22f0eb
Rodrigo Berasain <rodriberasain@gmail.com>
2024-09-03 13:44:11 -0300

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,28 +10,7 @@
 //
 //
 
-// -- This is a parent command --
-Cypress.Commands.add('getVerificationTokenByRequest', () => {
-  cy.request('/login?ReturnUrl=%2Fadmin%2F').then((loginResponse) => {
-    // Extract the verification token from the response body or headers
-    const verificationToken = loginResponse.body.match(/<input name="__RequestVerificationToken" type="hidden" value="([^"]+)" \/>/)[1];
-    return cy.wrap(verificationToken)
- })
-})
-
-Cypress.Commands.add('loginByRequest', (username, password, verificationToken) => {
-  cy.request({
-    method: 'POST',
-    url: '/login',
-    form: true,
-    body: {
-      Email: username,
-      Password: password,
-      __RequestVerificationToken: verificationToken,
-      RememberMe: false,
-    },
-  })
-})
+// -- This is a parent command -
 
 //
 //
